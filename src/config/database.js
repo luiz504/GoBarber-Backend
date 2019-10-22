6afed65b181643d2a8f9c340b38ed3a134fcb8d8
Luiz504 <luiz504@hotmail.com>
2019-10-22 17:11:14 -0300

    Base API express/axios/sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgress',
+  host: 'localhost',
+  username: 'postgress',
+  password: 'docker',
+  database: 'gobarber2',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
