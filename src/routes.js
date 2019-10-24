df0eb0f11e63481aeb30a91b49d12eabdff393f5
Luiz504 <luiz504@hotmail.com>
2019-10-24 11:24:43 -0300

    Chore: update account avatar / Db association

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import authenticantion from './app/middlewares/ authentication';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-
-import authenticantion from './app/middlewares/ authentication';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,8 +17,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authenticantion);
 routes.put('/users', authenticantion, UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
