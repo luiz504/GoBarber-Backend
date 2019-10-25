d72d2eabdc115aed62fbe11b9d117d10da2f2ae3
Luiz504 <luiz504@hotmail.com>
2019-10-25 13:36:27 -0300

    Feat: User cancel apponitment

--- src/routes.js
+++ src/routes.js
@@ -23,7 +23,9 @@ routes.put('/users', UserController.update); //
 
 routes.get('/providers', ProviderController.index); //
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store); //
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index); //
 
